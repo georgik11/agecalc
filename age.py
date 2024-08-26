3836796eaaba4bd1dc44e3ade135f5503955da25
georgik11 <georgik123@hotmail.com>
2024-08-26 17:44:35 +0400

    Modify current year

--- age.py
+++ age.py
@@ -1,9 +1,8 @@
-Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-... from datetime import datetime
-... bYear = int(input("Enter your birth year: "))
-... 
-... cYear = datetime.now().year
-... 
-... print(cYear)
+from datetime import datetime
+
+bYear = int(input("Enter your birth year: "))
+cYear = datetime.now().year
+
+print(cYear)
+
+print(2024 - bYear)
